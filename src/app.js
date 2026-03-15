8651e244615a7d429ae410340068d3a0b4325fa1
lakshmitanujakota <tanujakota14@gmail.com>
2026-03-15 22:28:01 +0530

    Multi-route handling

--- src/app.js
+++ src/app.js
@@ -1,12 +1,30 @@
-const express=require("express");
+const express = require("express");
 
-const app=express();
+const app = express();
 
-app.get("/tes?t",(req,res)=>{
-    res.send("Connected to test server.")
-});
+app.use("/use", (req, res, next) => {
+    console.log("response sending.");
+    next();
+    //res.send("Route  Server 1")
 
+},
+    (req, res, next) => {
+        console.log("response sending.");
+        //res.send("Route  Server 2")
+        next();
+    }, (req, res, next) => {
+        console.log("response sending.");
+        //res.send("Route  Server 3")
+        next();
+    }, (req, res, next) => {
+        console.log("response sending.");
+       // res.send("Route  Server 4")
+        next();
+    }, (req, res, next) => {
+        console.log("response sending.");
+        res.send("Route  Server 5")
+    })
 
-app.listen(7777, ()=>{
+app.listen(7777, () => {
     console.log("Server is cconnected.")
 });
