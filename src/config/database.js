b03b125ba741027d567bb6c416d18199702eff59
lakshmitanujakota <tanujakota14@gmail.com>
2026-03-21 19:39:48 +0530

    password

--- src/config/database.js
+++ src/config/database.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   await mongoose.connect(
-    "mongodb://tanujakota14_db_user:tanujaKo@ac-uj2jsaq-shard-00-00.mmgxm0z.mongodb.net:27017,ac-uj2jsaq-shard-00-01.mmgxm0z.mongodb.net:27017,ac-uj2jsaq-shard-00-02.mmgxm0z.mongodb.net:27017/devTinder?ssl=true&replicaSet=atlas-mavy2c-shard-0&authSource=admin&appName=NamesteNodeJs/devTinder"
+    "mongodb://*******************@ac-uj2jsaq-shard-00-00.mmgxm0z.mongodb.net:27017,ac-uj2jsaq-shard-00-01.mmgxm0z.mongodb.net:27017,ac-uj2jsaq-shard-00-02.mmgxm0z.mongodb.net:27017/devTinder?ssl=true&replicaSet=atlas-mavy2c-shard-0&authSource=admin&appName=NamesteNodeJs/devTinder"
   );
 };
 module.exports = { connectDB };
